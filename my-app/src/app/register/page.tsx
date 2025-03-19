51593baeb9bbabc4013c8e24699260b81298a2c3
Youssef-Mos <youssefmosbah04@gmail.com>
2025-03-20 00:28:26 +0100

    neuille

--- my-app/src/app/register/page.tsx
+++ my-app/src/app/register/page.tsx
@@ -16,15 +16,17 @@ export default function Register() {
             <div className="border-2 h-screen w-sm rounded-xl mt-4 hover:shadow-2xl transition-all duration-200 ease-in gap-5">
                 <h1 className='text-2xl md:text-4xl font-bold text-center mt-5 gap-5'>Créer votre compte :</h1>
                 <div className="flex flex-col items-center justify-center mt-7 gap-5">
-                    <div>
-                    <input type="radio" name="radio-1" className="radio" defaultChecked />
-                    <input type="radio" name="radio-1" className="radio" />
+                    <div className="flex gap-1 flex-col ">
+                        <div className="flex gap-2"><input type="radio" name="radio-1" className="radio-md cursor-pointer" /> <p>Masculin</p></div>
+                    
+                        <div className="flex gap-2"> <input type="radio" name="radio-1" className="radio-md cursor-pointer" /> <p>Feminin</p></div>
                     </div>
 
                     <div>
-                    <Label htmlFor="nom" className="flex items-start">Nom</Label>
-                    <Input type="nom" id="nom" placeholder="
-                    Votre nom" className="w-xs" />
+                        <label className="floating-label label">
+                            <span className="text-amber-500 bg- bg-amber-50">Your Email</span>
+                            <input type="text" placeholder="mail@site.com" className="input-ghost  input input-md w-xs rounded-lg  bg-amber-200" />
+                        </label>    
                     </div>
                     <div>
                     <Label htmlFor="prenom" className="flex items-start">Prénom</Label>
@@ -32,7 +34,6 @@ export default function Register() {
                     Votre prénom" className="w-xs" />
                     </div>
                 </div>
-
             </div>
             
         </div>
