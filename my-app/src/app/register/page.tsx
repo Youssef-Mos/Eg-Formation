e95a0965d67c004b577eb02d8018e01a57618eac
Youssef-Mos <youssefmosbah04@gmail.com>
2025-03-19 23:24:35 +0100

    daisy ui

--- my-app/src/app/register/page.tsx
+++ my-app/src/app/register/page.tsx
@@ -13,8 +13,25 @@ export default function Register() {
         
         <div className="flex justify-center items-center gap-5 flex-col">
             <Nav />
-            <div className="border-2 h-screen w-lg rounded-xl mt-4">
-                <h1 className='text-2xl md:text-4xl font-bold text-center mt-5'>Créer votre compte :</h1>
+            <div className="border-2 h-screen w-sm rounded-xl mt-4 hover:shadow-2xl transition-all duration-200 ease-in gap-5">
+                <h1 className='text-2xl md:text-4xl font-bold text-center mt-5 gap-5'>Créer votre compte :</h1>
+                <div className="flex flex-col items-center justify-center mt-7 gap-5">
+                    <div>
+                    <input type="radio" name="radio-1" className="radio" defaultChecked />
+                    <input type="radio" name="radio-1" className="radio" />
+                    </div>
+
+                    <div>
+                    <Label htmlFor="nom" className="flex items-start">Nom</Label>
+                    <Input type="nom" id="nom" placeholder="
+                    Votre nom" className="w-xs" />
+                    </div>
+                    <div>
+                    <Label htmlFor="prenom" className="flex items-start">Prénom</Label>
+                    <Input type="prenom" id="prenom" placeholder="
+                    Votre prénom" className="w-xs" />
+                    </div>
+                </div>
 
             </div>
             
